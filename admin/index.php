cbeb332db081cfc1591ac6d656f79a036a112150
mgalihpp <muhammadgalih451@gmail.com>
2024-11-06 10:38:59 +0700

    fix

--- admin/index.php
+++ admin/index.php
@@ -4,77 +4,79 @@ include '../config/db.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-	<title>Login | Admin</title>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-<!--===============================================================================================-->	
-	<link rel="icon" type="image/png" href="../assets/img/SMK.png"/>
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="../assets/_login/vendor/bootstrap/css/bootstrap.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="../assets/_login/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="../assets/_login/fonts/iconic/css/material-design-iconic-font.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="../assets/_login/vendor/animate/animate.css">
-<!--===============================================================================================-->	
-	<link rel="stylesheet" type="text/css" href="../assets/_login/vendor/css-hamburgers/hamburgers.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="../assets/_login/vendor/animsition/css/animsition.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="../assets/_login/vendor/select2/select2.min.css">
-<!--===============================================================================================-->	
-	<link rel="stylesheet" type="text/css" href="../assets/_login/vendor/daterangepicker/daterangepicker.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="../assets/_login/css/util.css">
-	<link rel="stylesheet" type="text/css" href="../assets/_login/css/main.css">
-<!--===============================================================================================-->
+    <title>Login | Admin</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--===============================================================================================-->
+    <link rel="icon" type="image/png" href="../assets/img/SMK.png" />
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="../assets/_login/vendor/bootstrap/css/bootstrap.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="../assets/_login/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="../assets/_login/fonts/iconic/css/material-design-iconic-font.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="../assets/_login/vendor/animate/animate.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="../assets/_login/vendor/css-hamburgers/hamburgers.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="../assets/_login/vendor/animsition/css/animsition.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="../assets/_login/vendor/select2/select2.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="../assets/_login/vendor/daterangepicker/daterangepicker.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="../assets/_login/css/util.css">
+    <link rel="stylesheet" type="text/css" href="../assets/_login/css/main.css">
+    <!--===============================================================================================-->
 </head>
+
 <body>
-	
-	<div class="limiter">
-		<div class="container-login100">
-			<div class="wrap-login100">
-				<form method="post" action="" class="login100-form validate-form">
-					<span class="login100-form-title p-b-40">
-						<!-- <i class="zmdi zmdi-font"></i> -->
-						<img src="../assets/img/SMK.png" width="250">
-					</span>
-					<span class="login100-form-title p-b-10">
-						SMKN1 Putussibau
-					</span>
-					
+
+    <div class="limiter">
+        <div class="container-login100">
+            <div class="wrap-login100">
+                <form method="post" action="" class="login100-form validate-form">
+                    <span class="login100-form-title p-b-40">
+                        <!-- <i class="zmdi zmdi-font"></i> -->
+                        <img src="../assets/img/SMK.png" width="250">
+                    </span>
+                    <span class="login100-form-title p-b-10">
+                        SMKN1 Putussibau
+                    </span>
 
 
-					<div class="wrap-input100 validate-input">
-						<input class="input100" type="text" name="username">
-						<span class="focus-input100" data-placeholder="Username"></span>
-					</div>
 
-					<div class="wrap-input100 validate-input" data-validate="password">
-						<span class="btn-show-pass">
-							<i class="zmdi zmdi-eye"></i>
-						</span>
-						<input class="input100" type="password" name="password">
-						<span class="focus-input100" data-placeholder="Password"></span>
-					</div>
+                    <div class="wrap-input100 validate-input">
+                        <input class="input100" type="text" name="username">
+                        <span class="focus-input100" data-placeholder="Username"></span>
+                    </div>
 
-					<div class="container-login100-form-btn">
-						<div class="wrap-login100-form-btn">
-							<div class="login100-form-bgbtn"></div>
-							<button type="submit" class="login100-form-btn">
-								Login
-							</button>
-						</div>
-					</div>
+                    <div class="wrap-input100 validate-input" data-validate="password">
+                        <span class="btn-show-pass">
+                            <i class="zmdi zmdi-eye"></i>
+                        </span>
+                        <input class="input100" type="password" name="password">
+                        <span class="focus-input100" data-placeholder="Password"></span>
+                    </div>
 
-				
-				</form>
+                    <div class="container-login100-form-btn">
+                        <div class="wrap-login100-form-btn">
+                            <div class="login100-form-bgbtn"></div>
+                            <button type="submit" class="login100-form-btn">
+                                Login
+                            </button>
+                        </div>
+                    </div>
 
-									<?php 
+
+                </form>
+
+                <?php 
 									if ($_SERVER['REQUEST_METHOD']=='POST') {
-									$pass= $_POST['password'];
+									$pass= sha1($_POST['password']);
 									$sqlCek = mysqli_query($con,"SELECT * FROM tb_admin WHERE username='$_POST[username]' AND password='$pass' AND aktif='Y'");
 									$jml = mysqli_num_rows($sqlCek);
 									$d = mysqli_fetch_array($sqlCek);
@@ -125,33 +127,34 @@ include '../config/db.php';
 									}
 									}
 									?>
-			</div>
-		</div>
-	</div>
-	
+            </div>
+        </div>
+    </div>
 
-	<div id="dropDownSelect1"></div>
-	
-<!--===============================================================================================-->
-	<script src="../assets/_login/vendor/jquery/jquery-3.2.1.min.js"></script>
-<!--===============================================================================================-->
-	<script src="../assets/_login/vendor/animsition/js/animsition.min.js"></script>
-<!--===============================================================================================-->
-	<script src="../assets/_login/vendor/bootstrap/js/popper.js"></script>
-	<script src="../assets/_login/vendor/bootstrap/js/bootstrap.min.js"></script>
-<!--===============================================================================================-->
-	<script src="../assets/_login/vendor/select2/select2.min.js"></script>
-<!--===============================================================================================-->
-	<script src="../assets/_login/vendor/daterangepicker/moment.min.js"></script>
-	<script src="../assets/_login/vendor/daterangepicker/daterangepicker.js"></script>
-<!--===============================================================================================-->
-	<script src="../assets/_login/vendor/countdowntime/countdowntime.js"></script>
-<!--===============================================================================================-->
 
-	<!-- Sweet Alert -->
-	<script src="../assets/js/plugin/sweetalert/sweetalert.min.js"></script>
-	<script src="../assets/_login/js/main.js"></script>
+    <div id="dropDownSelect1"></div>
+
+    <!--===============================================================================================-->
+    <script src="../assets/_login/vendor/jquery/jquery-3.2.1.min.js"></script>
+    <!--===============================================================================================-->
+    <script src="../assets/_login/vendor/animsition/js/animsition.min.js"></script>
+    <!--===============================================================================================-->
+    <script src="../assets/_login/vendor/bootstrap/js/popper.js"></script>
+    <script src="../assets/_login/vendor/bootstrap/js/bootstrap.min.js"></script>
+    <!--===============================================================================================-->
+    <script src="../assets/_login/vendor/select2/select2.min.js"></script>
+    <!--===============================================================================================-->
+    <script src="../assets/_login/vendor/daterangepicker/moment.min.js"></script>
+    <script src="../assets/_login/vendor/daterangepicker/daterangepicker.js"></script>
+    <!--===============================================================================================-->
+    <script src="../assets/_login/vendor/countdowntime/countdowntime.js"></script>
+    <!--===============================================================================================-->
+
+    <!-- Sweet Alert -->
+    <script src="../assets/js/plugin/sweetalert/sweetalert.min.js"></script>
+    <script src="../assets/_login/js/main.js"></script>
 
 
 </body>
+
 </html>
